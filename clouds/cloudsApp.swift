29cc80846e021b1d691ea3e24c4edb87546b93bb
Dennis Wilkins <dennis@inlineblock.com>
2022-11-11 16:42:56 -0800

    Initial Commit

---
+++ clouds/cloudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudsApp.swift
+//  clouds
+//
+//  Created by Dennis Wilkins on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct cloudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
